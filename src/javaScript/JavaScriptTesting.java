34ad1b55347cf61f89e78f73e8dceec15d8bf73e
schasins <schasins@gmail.com>
2013-11-19 18:25:58 -0800

    Function now takes arguments (from spreadsheet).

--- src/javaScript/JavaScriptTesting.java
+++ src/javaScript/JavaScriptTesting.java
@@ -2,11 +2,14 @@ package javaScript;
 
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.google.common.base.Joiner;
 
 import au.com.bytecode.opencsv.CSVReader;
 
@@ -15,21 +18,26 @@ public class JavaScriptTesting {
 	public static void main(String[] args) {
 		List<String[]> rows = new ArrayList<String[]>();
 		try {
-			CSVReader reader = new CSVReader(new FileReader("resources/input.csv"));
+			CSVReader reader = new CSVReader(new FileReader("resources/input.csv"), '\t');
 		    rows = reader.readAll();
 		}
 		catch(Exception e){
 			System.out.println("Failed to read input file");
 		}
 		
-		String javaScriptFunction = "return document.title;";
+		String javaScriptFunction = "var func = function(a,b){return document.title+' - '+a+' - '+b;};";
 		
 		WebDriver driver = new FirefoxDriver();
 		
 		for (int i = 0; i<rows.size(); i++){
-	        driver.get(rows.get(i)[0]);
 			if (driver instanceof JavascriptExecutor) {
-				Object ans = ((JavascriptExecutor) driver).executeScript(javaScriptFunction);
+				String[] row = rows.get(i);
+		        driver.get(row[0]);
+		        for(int j = 1; j < row.length; j++){
+		            row[j] = "'"+row[j]+"'";
+		        }
+				String argString = Joiner.on(",").join(Arrays.copyOfRange(row, 1, row.length));
+				Object ans = ((JavascriptExecutor) driver).executeScript(javaScriptFunction+" return func("+argString+");");
 				System.out.println(ans);
 			}
 		}
