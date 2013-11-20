c84eb35fea4e155c78aaf922cf4a7eac601afde1
schasins <schasins@gmail.com>
2013-11-19 18:33:54 -0800

    Now writes output to csv file.

--- src/javaScript/JavaScriptTesting.java
+++ src/javaScript/JavaScriptTesting.java
@@ -1,6 +1,7 @@
 package javaScript;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,21 +13,37 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import com.google.common.base.Joiner;
 
 import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
 
 public class JavaScriptTesting {
 	
 	public static void main(String[] args) {
+		//Input 1
 		List<String[]> rows = new ArrayList<String[]>();
 		try {
 			CSVReader reader = new CSVReader(new FileReader("resources/input.csv"), '\t');
 		    rows = reader.readAll();
 		}
 		catch(Exception e){
-			System.out.println("Failed to read input file");
+			System.out.println("Failed to open input file.");
+			return;
 		}
 		
+		//Input 2
 		String javaScriptFunction = "var func = function(a,b){return document.title+' - '+a+' - '+b;};";
 		
+		//Output
+		CSVWriter writer;
+		try{
+			String csv = "resources/output.csv";
+			writer = new CSVWriter(new FileWriter(csv));
+		}
+		catch(Exception e){
+			System.out.println("Failed to open output file.");
+			return;
+		}
+		
+		//Execution
 		WebDriver driver = new FirefoxDriver();
 		
 		for (int i = 0; i<rows.size(); i++){
@@ -38,10 +55,14 @@ public class JavaScriptTesting {
 		        }
 				String argString = Joiner.on(",").join(Arrays.copyOfRange(row, 1, row.length));
 				Object ans = ((JavascriptExecutor) driver).executeScript(javaScriptFunction+" return func("+argString+");");
-				System.out.println(ans);
+				
+				String [] ansArray = ans.toString().split("#"); 
+				writer.writeNext(ansArray);
 			}
 		}
-
+		
+		//Close output writer
+		try{writer.close();}catch(Exception e){System.out.println("Failed to close output file.");}
         //Close the browser
         driver.quit();
 	}
