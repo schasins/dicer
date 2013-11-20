1d899406a938ac75741822e810a623707a011272
schasins <schasins@gmail.com>
2013-11-19 17:51:01 -0800

    Version of the tester that takes input from spreadsheet.

--- src/javaScript/JavaScriptTesting.java
+++ src/javaScript/JavaScriptTesting.java
@@ -1,24 +1,33 @@
 package javaScript;
 
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import au.com.bytecode.opencsv.CSVReader;
+
 public class JavaScriptTesting {
 	
 	public static void main(String[] args) {
-		ArrayList<String> urls = new ArrayList<String>();
-		urls.add("http://google.com");
-		urls.add("http://facebook.com");
+		List<String[]> rows = new ArrayList<String[]>();
+		try {
+			CSVReader reader = new CSVReader(new FileReader("resources/input.csv"));
+		    rows = reader.readAll();
+		}
+		catch(Exception e){
+			System.out.println("Failed to read input file");
+		}
 		
 		String javaScriptFunction = "return document.title;";
 		
 		WebDriver driver = new FirefoxDriver();
 		
-		for (int i = 0; i<urls.size(); i++){
-	        driver.get(urls.get(i));
+		for (int i = 0; i<rows.size(); i++){
+	        driver.get(rows.get(i)[0]);
 			if (driver instanceof JavascriptExecutor) {
 				Object ans = ((JavascriptExecutor) driver).executeScript(javaScriptFunction);
 				System.out.println(ans);
