ef2965fff203944711fdd9e813a558f689c34dad
schasins <schasins@gmail.com>
2013-11-19 17:04:15 -0800

    Another failing example, using id.

--- src/javaScript/JavaScriptExample.java
+++ src/javaScript/JavaScriptExample.java
@@ -8,7 +8,7 @@ public class JavaScriptExample {
 	public static void main(String[] args) {
 		WebDriver driver = new FirefoxDriver();
 
-        // And now use this to visit Google
+        /*
         driver.get("http://www.eecs.berkeley.edu/~mangpo/www/home.html");
 			
 		if (driver instanceof JavascriptExecutor) {
@@ -17,6 +17,15 @@ public class JavaScriptExample {
 							//executeScript("return document.title;"));	
 							//executeScript("return document.getElementsByClassName(\'subscribe\').length;"));
 							executeScript("return document.getElementsByClassName(\'subscribe\')[0].innerText;"));
+		}
+		*/
+		
+        driver.get("http://www.cs.berkeley.edu/~bodik/");
+		
+		if (driver instanceof JavascriptExecutor) {
+			System.out.println(
+						((JavascriptExecutor) driver).
+							executeScript("return document.getElementById(\'footercont\').innerText;"));
 		}
 
         //Close the browser
