50d512c2b8e8a3a05d036b15b377d1a4f68ae982
schasins <schasins@gmail.com>
2013-12-04 17:41:32 -0800

    The basic getXpaths stage.

---
+++ resources/getXpaths.js
@@ -0,0 +1,35 @@
+var output = "";
+var url = "";
+var first = true;
+
+function walkTheDOM(node, xpath) {
+
+    if (!first){
+    	output+=("@#@"+url+"<,>"+xpath);
+    	}
+    else {
+       first = false;
+       output+=(url+"<,>"+xpath);
+    }
+    
+    node = node.firstChild;
+    var tags = {};
+    while (node) {
+        var tag = node.tagName;
+        if (!tag){
+           node = node.nextSibling;
+           continue;
+        }
+        tags[tag] = (tags[tag]) ? tags[tag]+1 : 1;
+        var xpathChild = xpath+"/"+tag+"["+tags[tag]+"]";
+        walkTheDOM(node,xpathChild);
+        node = node.nextSibling;
+    }
+}
+
+var func1 = function(urlArg){
+  url = urlArg;
+  var element = document.body;
+  walkTheDOM(element,"BODY");
+  return output;
+};
