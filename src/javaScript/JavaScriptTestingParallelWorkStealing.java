36a15beae6cab9e68bd0cf54ebf26512a9154969
schasins <schasins@gmail.com>
2013-12-06 16:41:46 -0800

    Now works not only for multiple subalgorithms, but also for multiple algorithms.  Uses row order to stitch rows together correctly.

--- src/javaScript/JavaScriptTestingParallelWorkStealing.java
+++ src/javaScript/JavaScriptTestingParallelWorkStealing.java
@@ -25,7 +25,8 @@ public class JavaScriptTestingParallelWorkStealing {
 	List<String[]> rows;
 	TaskQueue queue;
 	String javaScriptFunctions;
-	int functions;
+	int algorithms;
+	List<Integer> subalgorithms = new ArrayList<Integer>();
 	CSVWriter writer;
 	Boolean jquery;
 	
@@ -51,16 +52,30 @@ public class JavaScriptTestingParallelWorkStealing {
 			this.javaScriptFunctions = new Scanner(new File(javaScriptFile)).useDelimiter("\\Z").next();
 		}
 		catch(Exception e){System.out.println("Failed to open JavaScript input file."); return;}
-		this.functions = 0;
+		this.algorithms = 0;
+		//while loop for number of algorithms
 		while(true){
-			if(this.javaScriptFunctions.contains("var func"+(this.functions+1))){
-				this.functions++;
+			char letter = ((char) ((int) 'a' + this.algorithms));
+			if(this.javaScriptFunctions.contains("func_"+letter)){
+				this.algorithms++;
+				this.subalgorithms.add(0);
+				//while loop for number of subalgorithms for each algorithm
+				while(true){
+					int count = this.subalgorithms.get(this.algorithms-1);
+					if(this.javaScriptFunctions.contains("func_"+letter+(count+1))){
+						this.subalgorithms.set(this.algorithms-1, count+1);
+					}
+					else{
+						break;
+					}
+				}
 			}
 			else{
 				break;
 			}
 		}
-		System.out.println("functions: "+this.functions);
+		System.out.println("algorithms: "+this.algorithms);
+		System.out.println("subalgorithms: "+Arrays.toString(this.subalgorithms.toArray()));
 		
 		//Output
 		CSVWriter writer;
@@ -82,7 +97,7 @@ public class JavaScriptTestingParallelWorkStealing {
 		ArrayList<Thread> threadList = new ArrayList<Thread>();
 		
 		for (int i = 0; i < threads; i++){
-			RunTests r = new RunTests(this.queue,this.javaScriptFunctions, this.functions, this.writer, this.jquery);
+			RunTests r = new RunTests(this.queue,this.javaScriptFunctions, this.algorithms, this.subalgorithms, this.writer, this.jquery);
 	        Thread t = new Thread(r);
 	        threadList.add(t);
 	        t.start();
@@ -129,16 +144,18 @@ public class JavaScriptTestingParallelWorkStealing {
 	private static class RunTests implements Runnable {
 		TaskQueue queue;
 		String javaScriptFunction;
-		int functions;
+		int algorithms;
+		List<Integer> subalgorithms;
 		CSVWriter writer;
 		Boolean jquery;
 		Boolean verbose = true;
 		
-		RunTests(TaskQueue queue, String javaScriptFunction, int functions, CSVWriter writer, Boolean jquery){
+		RunTests(TaskQueue queue, String javaScriptFunction, int algorithms, List<Integer> subalgorithms, CSVWriter writer, Boolean jquery){
 			this.queue = queue;
 			this.javaScriptFunction = javaScriptFunction;
 			this.writer = writer;
-			this.functions = functions;
+			this.algorithms = algorithms;
+			this.subalgorithms = subalgorithms;
 			this.jquery = jquery;
 		}
 		
@@ -173,40 +190,56 @@ public class JavaScriptTestingParallelWorkStealing {
 					}
 					String url = row[0];
 					if (!url.startsWith("http")){url = "http://"+url;}
-			        driver.get(url);
-			        
-			        //make the argString, since that will be the same across subalgorithms
+
+			        //make the argString, since that will be the same across algorithms and subalgorithms
 			        for(int j = 0; j < row.length; j++){
 			            row[j] = "'"+row[j]+"'";
 			        }
 					String argString = Joiner.on(",").join(Arrays.copyOfRange(row, 0, row.length));
+
+			        List<String> ansList = new ArrayList<String>();
 					
-			        List<List<String>> ansList = new ArrayList<List<String>>();
-			        for(int i = 0; i<this.functions; i++){
-				        //load jquery if we need it and if we're on a new page
-				        if (this.jquery){
-					        String jqueryCode;
-					        try{
-								jqueryCode = new Scanner(new File("resources/jquery-1.10.2.min.js")).useDelimiter("\\Z").next();
-							}
-							catch(Exception e){System.out.println("Failed to open jquery file."); return;}
-					        ((JavascriptExecutor) driver).executeScript(jqueryCode);
-				        }
-				        
-				        //System.out.println(this.javaScriptFunction+" return func"+(i+1)+"("+argString+");");
-				        Object ans = ((JavascriptExecutor) driver).executeScript(this.javaScriptFunction+" return func"+(i+1)+"("+argString+");");
-						
-						
-						if(i == (this.functions-1)){
-							if (ans != null){
-								ArrayList<String> ansRows = new ArrayList<String>(Arrays.asList(ans.toString().split("@#@")));
-								for(int j = 0; j<ansRows.size(); j++){
-									String ansRow = ansRows.get(j);
-									//if(this.verbose){System.out.println(ansRow);}
-									this.writer.writeNext(ansRow.split("<,>"));
+			        for (int j = 0; j<this.algorithms; j++){
+				        //reload for each algorithm
+			        	driver.get(url);
+
+						char letter = ((char) ((int) 'a' + j));
+				        int algorithmSubalgorithms = this.subalgorithms.get(j);
+				        for(int i = 0; i<algorithmSubalgorithms; i++){
+					        //load jquery if we need it and if we're on a new page
+					        if (this.jquery){
+						        String jqueryCode;
+						        try{
+									jqueryCode = new Scanner(new File("resources/jquery-1.10.2.min.js")).useDelimiter("\\Z").next();
+								}
+								catch(Exception e){System.out.println("Failed to open jquery file."); return;}
+						        ((JavascriptExecutor) driver).executeScript(jqueryCode);
+					        }
+					        
+					        //System.out.println(this.javaScriptFunction+" return func"+(i+1)+"("+argString+");");
+					        Object ans = ((JavascriptExecutor) driver).executeScript(this.javaScriptFunction+" return func_"+letter+(i+1)+"("+argString+");");
+							
+							
+							if(i == (algorithmSubalgorithms-1)){
+								if (ans != null){
+									ArrayList<String> ansRows = new ArrayList<String>(Arrays.asList(ans.toString().split("@#@")));
+									for(int k = 0; k<ansRows.size(); k++){
+										String ansRow = ansRows.get(k);
+										if (ansList.size()>k){
+											ansList.set(k, ansList.get(k)+"<,>"+ansRow);
+										}
+										else{
+											ansList.add(ansRow);
+										}
+										//if(this.verbose){System.out.println(ansRow);}
+									}
 								}
 							}
-						}
+				        }
+			        }
+			        //put anslist in the writer
+			        for(int i = 0; i<ansList.size(); i++){
+			        	this.writer.writeNext(ansList.get(i).split("<,>"));
 			        }
 				}
 			}
@@ -229,14 +262,19 @@ public class JavaScriptTestingParallelWorkStealing {
 		String javaScript3 = "resources/nodeSaving.js";
 		String output3 = "resources/output3.csv";
 		
+		String input4 = "resources/output3.csv";
+		String javaScript4 = "resources/nodeRetrieving.js";
+		String output4 = "resources/output4.csv";
+		
 		Boolean jquery = false;
 		int threads = 4;
 		
 		JavaScriptTestingParallelWorkStealing system = new JavaScriptTestingParallelWorkStealing();
 		system.startSession();
-		system.stage(input1,javaScript1,output1,jquery,threads);
-		system.stage(input2,javaScript2,output2,jquery,threads);
-		system.stage(input3,javaScript3,output3,jquery,threads);
+		//system.stage(input1,javaScript1,output1,jquery,threads);
+		//system.stage(input2,javaScript2,output2,jquery,threads);
+		//system.stage(input3,javaScript3,output3,jquery,threads);
+		system.stage(input4,javaScript4,output4,jquery,threads);
 		system.endSession();
 	}
 
