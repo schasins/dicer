12cb9fb14f0a46d6a0adcf4f447204e997ed8679
schasins <schasins@gmail.com>
2015-12-07 14:27:17 -0800

    No more stage 5.

--- src/javaScript/NodeAddressingComparison.java
+++ src/javaScript/NodeAddressingComparison.java
@@ -23,9 +23,6 @@ public class NodeAddressingComparison {
 		
 		String javaScript4 = "resources/programs/nodeAddressingComparison/pldi-nodeRetrieving.js";
 		String output4Start = "resources/tables/nodeAddressingComparison/stage4-nodeRetrieval";
-
-		String javaScript5 = "resources/programs/nodeAddressingComparison/pldi-similarityMetricDistance.js";
-		String output5Start = "resources/tables/nodeAddressingComparison/stage5-similarityMetricDistance";
 		
 		Boolean jquery = true;
 		int threads = 32;
@@ -43,22 +40,18 @@ public class NodeAddressingComparison {
 		if (firstSession){
 			String date = new SimpleDateFormat("dd-MM-yyyy-hh-mm").format(new Date());
 			String output4 = output4Start+"_"+date+".csv";
-			String output5 = output5Start+"_"+date+".csv";
 			system.startSession();
 			system.stage(input1,javaScript1,output1,false,threads,200,false,"");
 			system.stage(output1,javaScript2,output2,false,threads,400,false,"");
 			system.stage(output2,javaScript3,output3,true,threads,400,false,"");
 			system.stage(output3,javaScript4,output4,true,threads,400,false,"");
-			system.stage(output4,javaScript5,output5,false,threads,1400,false,"");
 			system.endSession();
 		}
 		//run a test even if we're doing training, so we have a test very shortly after training
 		String date = new SimpleDateFormat("dd-MM-yyyy-hh-mm").format(new Date());
 		String output42 = output4Start+"_"+date+".csv";
-		String output52 = output5Start+"_"+date+".csv";
 		system.startSession();
 		system.stage(output3,javaScript4,output42,true,threads,400,false,"");
-		system.stage(output42,javaScript5,output52,false,threads,1400,false,"");
 		system.endSession();
 	}
 
